b190b571a80c8e578a9aa45a54b99443be23e4b8
tsikuzro <tsikuzro1@gmail.com>
2021-05-18 05:50:59 +0200

    Add existing files

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 function compute()
 {
-    var princ = document.getElementById("principal").value;
+    var p = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    interest = (p * years * rate)/100;
+    var year = new Date().getFullYear() + parseInt(years);
 
-    if (princ ==""){
+    if (p == "" || p <="0"){
         alert("Enter a positive number");
-        princ.focus();
-        return false;
-    }else if (princ <="0"){
-        alert("Enter a positive number");
-        princ.focus();
+        document.getElementById("principal").focus();
         return false;
     }else{
-        var principal = document.getElementById("principal").value;
-        var rate = document.getElementById("rate").value;
-        var years = document.getElementById("years").value;
-        var interest = principal * years * rate /100;
-        var year = new Date().getFullYear() + parseInt(years);
-
-        document.getElementById("result").innerText="If you deposit "+ principal + ",\n"+ "at an interest rate of "+ rate +"%.\n"+ "You will receive an amount of "+ interest +",\n"+ "in the year "+ year;
+        span = 'If you deposit <mark>' + p + '</mark>, <br/>at an interest rate of <mark>' + rate + '%.</mark> <br/>You will recieve an amount of <mark>'+ interest + '</mark>, <br/>in the year <mark>'+ year +'</mark>'; 
+     	document.getElementById("result").innerHTML=span;
     }
     return true; 
 }
